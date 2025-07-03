f134d806a25660549e223a0065d982d882a0af38
Shadab007-byte <shadabkhanmnw349@gmail.com>
2025-07-03 12:54:39 +0530

    Tested f(t, y) function with sample input t = 0.0, y = 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+fn f(t : f64 , y : f64 ) -> f64 {
+    t.cos() - y
+}
+
+
 fn main() {
-    println!("Euler IVP Solver in Rust");
+   let t = 0.0 ;
+   let y = 1.0 ;
+
+    println!("f(t = {t} , y ={y}) = {}" , f(t,y)) ;
 }
