117682ba0126d372282b98674df6815f1443586e
Shadab007-byte <shadabkhanmnw349@gmail.com>
2025-07-05 01:20:30 +0530

    Compared Euler method with analytical solution and added plot + CSV output

--- src/main.rs
+++ src/main.rs
@@ -28,22 +28,31 @@ fn euler_solver(f : fn(f64,f64) -> f64 , t0 : f64 , y0: f64 , t_end : f64 , n : 
 
 }
 
+// Analytic solution is :
 
-fn save_to_csv(t_vals: &Vec<f64>, y_vals: &Vec<f64>, filename: &str) {
+fn analytical_solution(t: f64) -> f64 {
+    0.5 * (t.cos() + t.sin()) + 0.5 * (-t).exp()
+}
+
+
+fn save_comparison_csv(t_vals: &Vec<f64>, y_numerical: &Vec<f64>, filename: &str) {
     let file = File::create(filename).expect("Unable to create file");
     let mut writer = BufWriter::new(file);
 
-    writeln!(writer, "t,y").unwrap(); // header
-    for (t, y) in t_vals.iter().zip(y_vals.iter()) {
-        writeln!(writer, "{},{}", t, y).unwrap();
+    writeln!(writer, "t,y_numerical,y_analytical,error").unwrap();
+    for (t, y_num) in t_vals.iter().zip(y_numerical.iter()) {
+        let y_exact = analytical_solution(*t);
+        let error = (y_num - y_exact).abs();
+        writeln!(writer, "{},{},{},{}", t, y_num, y_exact, error).unwrap();
     }
 }
+
 
 fn main() {
    let t0 = 0.0;
    let y0 = 1.0;
    let t_end = 5.0;
-   let n = 20;
+   let n = 1000;
    
    let (t_vals, y_vals) = euler_solver(f, t0, y0, t_end, n);
 
@@ -52,7 +61,7 @@ fn main() {
     }
 
      // Export to CSV
-    save_to_csv(&t_vals, &y_vals, "rust_results.csv");
-    println!("Results saved to rust_results.csv");
+    save_comparison_csv(&t_vals, &y_vals, "euler_vs_exact.csv");
+    println!("Comparison saved to euler_vs_exact.csv");
     
 }
